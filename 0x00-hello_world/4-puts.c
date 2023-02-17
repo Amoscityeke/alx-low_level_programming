4d64badf2ec2aa0ac03eab9374ea528de285c3af
Amoscityeke <ekeamos@gmail.com>
2023-02-17 00:18:31 -0800

    Hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	puts("\"programming is like building a multilingiual puzzel);
+
+		return (0);
+
+}
+
