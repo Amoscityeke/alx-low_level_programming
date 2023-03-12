97c54ff87c74826de7c469ccd5bbb0392c75d43c
Amoscityeke <ekeamos@gmail.com>
2023-03-12 15:29:49 -0700

    printing of number argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - printing the number of argument passed
+ * @argc: number parameter
+ * @argv:array parameter
+ * Return:  0 if successful
+ */
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
