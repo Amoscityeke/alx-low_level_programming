c55a74a9809db056daf4d3c604f5d4fcb6eee3b8
Amoscityeke <ekeamos@gmail.com>
2023-03-12 18:20:05 -0700

    multiplying two number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
+
 /**
  * main - multiplication of two argument
  * @argc: number parameter
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 	}
 	for (num1 = 1; num1 < argc; num1++)
 	{
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	mult = num1 * num2;
 	printf("%d\n", mult);
 	}
